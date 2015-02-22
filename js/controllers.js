d7d80373afda4f18d0808e844d2b73ad14fde589
Niyaaz <niyaazparker@gmail.com>
2015-02-22 17:32:53 +0200

    json link to file

--- js/controllers.js
+++ js/controllers.js
@@ -1,42 +1,13 @@
 var samewaveapp = angular.module('samewaveapp', []);
 
-samewaveapp.controller ('samewavecontroller', function samewavecontroller($scope) {
-			$scope.samewaveinfo = [
-			{
-				"id": 1,
-				"title": "Sales Target",
-				"owner": "Holly Garner"
-			},
-			{
+samewaveapp.controller ('samewavecontroller', ['$scope', '$http', function($scope, $http) 
+				{
+			$http.get('js/samewave_tech_test.json').success(function(data){
+				$scope.samewaveinfo = data;
+				});
+				
+				}]
 			
-				"id": 2,
-				"title": "Weight Loss",
-				"owner": "Marc Houston"
-			},
-			{
-				"id": 3,
-				"title": "Engineer Calls",
-				"owner": "Jennifer Hardy"
-			},
-			{
-				"id": 4,
-				"title": "Open Complaints",
-				"owner": "George Roberts"
-			},
-			{
-				"id": 5,
-				"title": "Bank Balance",
-				"owner": "Justin Ross"
-			},
-			{
-				"id": 6,
-				"title": "Complaints Received",
-				"owner": "Scott Lane"
-			},
-			{
-				"id": 7,
-				"title": "Fuel Reserves",
-				"owner": "Verna Patton"
-			}	
-		]
-	});
+			);
+
+
