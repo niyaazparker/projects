1ea420b281102127f5d2c310ca07c5a8b808c398
Niyaaz <niyaazparker@gmail.com>
2015-02-22 15:02:09 +0200

    first samewave commit

---
+++ js/controllers.js
@@ -0,0 +1,42 @@
+var samewaveapp = angular.module('samewaveapp', []);
+
+samewaveapp.controller ('samewavecontroller', function samewavecontroller($scope) {
+			$scope.samewaveinfo = [
+			{
+				"id": 1,
+				"title": "Sales Target",
+				"owner": "Holly Garner"
+			},
+			{
+			
+				"id": 2,
+				"title": "Weight Loss",
+				"owner": "Marc Houston"
+			},
+			{
+				"id": 3,
+				"title": "Engineer Calls",
+				"owner": "Jennifer Hardy"
+			},
+			{
+				"id": 4,
+				"title": "Open Complaints",
+				"owner": "George Roberts"
+			},
+			{
+				"id": 5,
+				"title": "Bank Balance",
+				"owner": "Justin Ross"
+			},
+			{
+				"id": 6,
+				"title": "Complaints Received",
+				"owner": "Scott Lane"
+			},
+			{
+				"id": 7,
+				"title": "Fuel Reserves",
+				"owner": "Verna Patton"
+			}	
+		]
+	});
